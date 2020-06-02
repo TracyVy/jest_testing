7bc40dcfe34a2172ef9ba8092bf75e0d3877d070
Tracy Vy <tracyvy88@gmail.com>
2020-06-01 19:27:21 -0700

    testing

--- test/util.test.js
+++ test/util.test.js
@@ -1,7 +1,18 @@
-const { add } = require("../util/util");
+const { add, minus } = require("../util/util");
 
 describe("Add", () => {
-  it("Should return the sum of the 2 passed arguments", () => {
-    expect(add(2, 2)).toBe(4);
+  describe("Success", () => {
+    it("Should return the sum of the 2 passed arguments", () => {
+      expect(add(2, 2)).toBe(4);
+    });
+  });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => add("2", 2)).toThrowError();
+    });
+    it("Should throw an error if the second argument is not a number", () => {
+      expect(() => add(2, "2")).toThrowError();
+    });
   });
 });
