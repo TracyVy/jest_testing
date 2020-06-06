271fb547636885e4cc99e56215ebd449524ec99a
Tracy Vy <tracyvy88@gmail.com>
2020-06-06 11:24:34 -0700

    second commit

--- util/util.js
+++ util/util.js
@@ -6,6 +6,12 @@ const add = (num1, num2) => {
   return num1 + num2;
 };
 
-const minus = (num1, num2) => {};
+const minus = (num1, num2) => {
+  if (typeof num1 != "number")
+    throw new Error("First argument must be a number");
+  if (typeof num2 != "number")
+    throw new Error("Second argument must be a number");
+  return num1 - num2;
+};
 
 module.exports = { add, minus };
