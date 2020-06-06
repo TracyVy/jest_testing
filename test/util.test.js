2f349e51eabce25fbd6e432b7f72a5b00ae4b997
Tracy Vy <tracyvy88@gmail.com>
2020-06-06 13:47:41 -0700

    add more test functions

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-const { add, minus } = require("../util/util");
+const { add, minus, multiply, divide, power } = require("../util/util");
 
 // describe f(n) is built into jest.
 describe("Add", () => {
@@ -36,6 +36,63 @@ describe("Minus", () => {
     });
     it("Should throw an error if no arguments were received", () => {
       expect(() => minus()).toThrowError();
+    });
+  });
+});
+
+describe("Multiply", () => {
+  describe("Success", () => {
+    it("Should return the first argument minus the second", () => {
+      expect(multiply(4, 2)).toBe(8);
+    });
+  });
+  describe("Fail", () => {
+    it("Should throw an error if 1st argument isn't a number", () => {
+      expect(() => multiply("2", 2)).toThrowError();
+    });
+    it("Should throw an error if 2nd argument isn't a number", () => {
+      expect(() => multiply(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => multiply()).toThrowError();
+    });
+  });
+});
+
+describe("Divide", () => {
+  describe("Success", () => {
+    it("Should return the first argument minus the second", () => {
+      expect(divide(4, 2)).toBe(2);
+    });
+  });
+  describe("Fail", () => {
+    it("Should throw an error if 1st argument isn't a number", () => {
+      expect(() => divide("2", 2)).toThrowError();
+    });
+    it("Should throw an error if 2nd argument isn't a number", () => {
+      expect(() => divide(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => divide()).toThrowError();
+    });
+  });
+});
+
+describe("Power", () => {
+  describe("Success", () => {
+    it("Should return the first argument minus the second", () => {
+      expect(power(4, 2)).toBe(16);
+    });
+  });
+  describe("Fail", () => {
+    it("Should throw an error if 1st argument isn't a number", () => {
+      expect(() => power("2", 2)).toThrowError();
+    });
+    it("Should throw an error if 2nd argument isn't a number", () => {
+      expect(() => power(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => power()).toThrowError();
     });
   });
 });
