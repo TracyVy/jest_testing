2170be73c71dbc9d92c99d5c55cf7e267493c3b3
Tracy Vy <tracyvy88@gmail.com>
2020-06-06 12:49:13 -0700

    add more test

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,6 @@
 const { add, minus } = require("../util/util");
 
+// describe f(n) is built into jest.
 describe("Add", () => {
   describe("Success", () => {
     it("Should return the sum of the 2 passed arguments", () => {
@@ -8,11 +9,33 @@ describe("Add", () => {
   });
 
   describe("Fail", () => {
-    it("Should throw an error if the first argument is not a number", () => {
+    it("Should throw an error if 1st argument isn't a number", () => {
       expect(() => add("2", 2)).toThrowError();
     });
-    it("Should throw an error if the second argument is not a number", () => {
+    it("Should throw an error if 2nd argument isn't a number", () => {
       expect(() => add(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => add()).toThrowError();
+    });
+  });
+});
+
+describe("Minus", () => {
+  describe("Success", () => {
+    it("Should return the first argument minus the second", () => {
+      expect(minus(4, 2)).toBe(2);
+    });
+  });
+  describe("Fail", () => {
+    it("Should throw an error if 1st argument isn't a number", () => {
+      expect(() => minus("2", 2)).toThrowError();
+    });
+    it("Should throw an error if 2nd argument isn't a number", () => {
+      expect(() => minus(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => minus()).toThrowError();
     });
   });
 });
